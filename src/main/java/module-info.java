6d06d3e6a5a017ca7ba7b86b2dc145757979b6fb
Abdelmoughit Moubarik <Abdelmoughit.Moubarik@emsi-edu.ma>
2023-01-04 15:10:03 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.nixgym {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.nixgym to javafx.fxml;
+    exports com.example.nixgym;
+}
